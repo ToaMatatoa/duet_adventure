273a07c2d722727c6acb6d1e34c493ff6da10366
Dmitriy Stupak <ds@bam-boo.eu>
2026-03-24 12:09:43 +0200

    SA-18: cleanup profile screen

--- composeApp/src/commonMain/kotlin/com/sexadventure/presentation/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/sexadventure/presentation/profile/ProfileScreen.kt
@@ -3,7 +3,6 @@ package com.sexadventure.presentation.profile
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,15 +12,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sexadventure.designsystem.listitem.ListItem
@@ -44,26 +43,49 @@ fun ProfileScreen(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-                .fillMaxSize()
-                .background(color = MaterialTheme.colorScheme.background)
-                .padding(horizontal = 16.dp, vertical = 8.dp),
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
     ) {
         TopBar(
             title = Strings.Profile.SCREEN_TITLE,
             showBackButton = false,
         )
+        
+        RandomPoseElement(
+            state = state,
+            onGetRandomPose = onGetRandomPose,
+            onFavouriteClick = onFavouriteClick,
+            onOpenPoseDetails = onOpenPoseDetails,
+            onRemovePose = onRemovePose,
+            modifier = Modifier.padding(top = 16.dp),
+        )
 
-        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+
+    }
+}
+
+@Composable
+private fun RandomPoseElement(
+    state: ProfileState,
+    onGetRandomPose: () -> Unit,
+    onFavouriteClick: (id: Int, currentFavourite: Boolean) -> Unit,
+    onOpenPoseDetails: (Int) -> Unit,
+    onRemovePose: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
             TextButton(
                 onClick = onGetRandomPose,
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.textButtonColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                ),
                 modifier = Modifier
-                        .fillMaxWidth()
-                        .height(height = 56.dp)
-                        .weight(weight = 1f)
-                        .background(
-                            color = MaterialTheme.colorScheme.surface,
-                            shape = MaterialTheme.shapes.medium,
-                        ),
+                    .fillMaxWidth()
+                    .height(height = 56.dp)
+                    .weight(weight = 1f),
             ) {
                 Text(
                     text = Strings.Profile.GET_RANDOM_POSE,
@@ -74,16 +96,14 @@ fun ProfileScreen(
 
             AnimatedVisibility(visible = state.pose != null) {
                 Box(
-                    modifier = Modifier
-                        .size(size = 56.dp)
-                        .background(
-                            color = MaterialTheme.colorScheme.surface,
-                            shape = MaterialTheme.shapes.medium,
-                        ).clickable(
-                            interactionSource = remember { MutableInteractionSource() },
-                            indication = ripple(bounded = false, radius = 32.dp),
-                            onClick = onRemovePose,
-                        ),
+                    modifier =
+                        Modifier
+                            .size(size = 56.dp)
+                            .clip(shape = MaterialTheme.shapes.medium)
+                            .background(
+                                color = MaterialTheme.colorScheme.surface,
+                                shape = MaterialTheme.shapes.medium,
+                            ).clickable(onClick = onRemovePose),
                 ) {
                     Icon(
                         imageVector = TablerIcons.X,
