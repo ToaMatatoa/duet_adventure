813b3964dc979aa59a819bf88210e94e3b11ce11
Dmitriy Stupak <ds@bam-boo.eu>
2026-03-24 19:36:27 +0200

    SA-18: part of images for predefined poses

--- composeApp/src/commonMain/kotlin/com/sexadventure/mapper/ImageMapper.kt
+++ composeApp/src/commonMain/kotlin/com/sexadventure/mapper/ImageMapper.kt
@@ -2,7 +2,15 @@ package com.sexadventure.mapper
 
 import org.jetbrains.compose.resources.DrawableResource
 import sexadventure.composeapp.generated.resources.Res
+import sexadventure.composeapp.generated.resources.cowgirl
+import sexadventure.composeapp.generated.resources.doggy_style
+import sexadventure.composeapp.generated.resources.lotus
+import sexadventure.composeapp.generated.resources.missionary
+import sexadventure.composeapp.generated.resources.reverse_cowgirl
 import sexadventure.composeapp.generated.resources.sixty_nine
+import sexadventure.composeapp.generated.resources.spooning
+import sexadventure.composeapp.generated.resources.standing
+import sexadventure.composeapp.generated.resources.the_bridge
 
 /**
  * Maps a pose image name (stored in JSON / Room) to a local [DrawableResource].
@@ -19,12 +27,18 @@ import sexadventure.composeapp.generated.resources.sixty_nine
  * 1. Drop the `.webp` file into `composeResources/drawable/`
  * 2. Add a mapping entry below
  */
-private val imageMap: Map<String, DrawableResource> = mapOf(
-    "sixty_nine" to Res.drawable.sixty_nine,
-    // "missionary" to Res.drawable.missionary,
-    // "cowgirl" to Res.drawable.cowgirl,
-    // ... add all 72 poses here after dropping the .webp files
-)
+private val imageMap: Map<String, DrawableResource> =
+    mapOf(
+        "missionary" to Res.drawable.missionary,
+        "cowgirl" to Res.drawable.cowgirl,
+        "reverse_cowgirl" to Res.drawable.reverse_cowgirl,
+        "doggy_style" to Res.drawable.doggy_style,
+        "spooning" to Res.drawable.spooning,
+        "sixty_nine" to Res.drawable.sixty_nine,
+        "lotus" to Res.drawable.lotus,
+        "standing" to Res.drawable.standing,
+        "the_bridge" to Res.drawable.the_bridge,
+    )
 
 /**
  * Resolves a pose image name to a [DrawableResource].
@@ -34,4 +48,3 @@ fun resolveImage(imageName: String?): DrawableResource? {
     if (imageName.isNullOrBlank()) return null
     return imageMap[imageName]
 }
-
